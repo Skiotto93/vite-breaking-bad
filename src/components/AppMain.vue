e2a6290bf9fde0f8e38810220d212721a40b2d60
skiotto93 <fabio.forenza@hotmail.com>
2022-11-29 19:56:16 +0100

    Aggiunto contatore

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,11 +1,13 @@
 <script>
 import AppCard from './AppCard.vue'
 import axios from 'axios'
+import { store } from '../store.js'
 
 export default {
     name: 'AppMain',
     components: {
         AppCard,
+        store,
     },
     data() {
         return {
@@ -18,20 +20,27 @@ export default {
             console.log(resp);
             this.characters = resp.data;
         });
+        
     },
+    computed() {
+
+    }
 }
 </script>
 
 <template>
-    <form action="">
-        <select class="choice" name="category">
+    <form>
+        <select class="choice" name="category" >
             <option selected>Select Category</option>
             <option value="Breaking Bad">Breaking Bad</option>
             <option value="Better Call Saul">Better Call Saul</option>
         </select>
     </form>
     <section class="container">
-        <AppCard v-for="character in characters" :info="character"/>
+        <div class="count">Found {{ characters.length }} characters</div>
+        <div class="d-flex">
+            <AppCard v-for="character in characters" :info="character"/>
+        </div>
     </section>
 </template>
 
@@ -47,8 +56,17 @@ export default {
     margin: auto;
     background-color: #fff;
     padding: 20px;
+    
+}
+.d-flex {
     display: flex;
     flex-wrap: wrap;
 }
-
+.count {
+    width: 100%;
+    background-color: #212529;
+    padding: 20px;
+    color: #fff;
+    margin-bottom: 15px;
+}
 </style>
